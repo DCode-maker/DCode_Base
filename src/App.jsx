d9529a29281a56cb512f2502ef39ce6aef712082
Dharmendar Kumar <dharmendar.kumar@fourbrick.com>
2024-05-25 20:10:20 +0530

    Adding First Code

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './index.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className='w-screen h-screen text-center  text-2xl'>DCode_Base</div>
+    </>
+  )
+}
+
+export default App
